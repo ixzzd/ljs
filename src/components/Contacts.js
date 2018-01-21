6ec63dc8f1368a27b872265a98b3700d62cdb441
Egor Chemokhonenko <ec@servers.com>
2018-01-21 08:45:18 +0200

    add components

---
+++ src/components/Contacts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Contacts extends React.Component {
+  render() {
+    return (
+      <div>
+        Contacts
+      </div>
+    );
+  }
+}
---
+++ src/components/ModelsFlow/Model/ContentItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import LazyLoad from 'react-lazyload';
+import ReactPlayer from 'react-player'
+
+export default class ContentItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.content = this.props.content
+  }
+
+  render() {
+    return (
+      <LazyLoad offset={100} once>
+        <div className='item'>
+          {this.content.type.toLowerCase() == 'video' ? (
+              <ReactPlayer url={this.content.source} controls={true} />
+            ) : (
+              <img key={this.content.id} src={this.content.image_url.normal}/>
+            )
+          }
+          <span> {this.content.description} </span>
+        </div>
+      </LazyLoad>
+    );
+  }
+}

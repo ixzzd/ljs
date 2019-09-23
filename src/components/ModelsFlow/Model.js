e8317c1187127c5995d5b5bcfc39c01cf4963a26
Egor Chemokhonenko <egor.chemohonenko@ahrefs.com>
2019-09-23 23:34:57 +0300

    add movies

--- src/components/ModelsFlow/Model.js
+++ src/components/ModelsFlow/Model.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavLink from 'react-router-dom'
 import ProgressiveImage from 'react-progressive-image'
 import Description from './Model/Description';
-import ContentItem from './Model/ContentItem';
+import MediaItem from './Model/MediaItem';
 import { observer, inject } from 'mobx-react';
 
 @inject("store")
@@ -16,7 +16,7 @@ export default class Model extends React.Component {
     this.selectImage = this.selectImage.bind(this)
     this.imageClass = this.imageClass.bind(this)
     this.handleImageLoaded = this.handleImageLoaded.bind(this)
-    this.state = {selectedImage: this.model.face, faceImageLoaded: false}
+    this.state = {selectedImage: this.model.face, faceImageLoaded: false, tab: ''}
   }
 
   selectImage(image, e) {
@@ -25,15 +25,27 @@ export default class Model extends React.Component {
     })
   }
 
+  selectTab(tab, e) {
+    this.setState({
+      tab
+    })
+  }
+
   handleImageLoaded() {
     this.setState({ faceImageLoaded: true });
   }
+
 
   imageClass(image) {
     if (this.state.selectedImage == image) return 'bordered'
   }
 
+
   render() {
+    const media = this.model.contents.filter(content => ['photo', 'video'].includes(content.type.toLowerCase()));
+    // const press = this.model.contents.filter(content => 'press' == content.type.toLowerCase());
+    const movies = this.model.contents.filter(content => 'movie' == content.type.toLowerCase());
+
     return (
       <div>
         {(this.props.index >= this.store.currentModelIndex - 1 && this.props.index < this.store.currentModelIndex + 2) &&
@@ -67,17 +79,50 @@ export default class Model extends React.Component {
                 { this.model.size.length > 0 &&
                     <p>{this.model.size}</p>
                 }
+                {movies.length > 0 &&
+                  <div className='movies'>
+                    <p className='moviesTitle'>MOVIES</p>
+                    {movies.map((movie, index) => (
+                      <p key={index}>
+                        {movie.description}
+                      </p>
+                    ))}
+                  </div>}
               </div>
             </div>
           </div>
         }
         { this.props.index == this.store.currentModelIndex &&
             this.state.faceImageLoaded &&
-            <div className='contents'>
-              {this.model.contents.map((content, index) => (
-                <ContentItem key={index} content={content} />
-              ))}
-            </div>
+              <div>
+                {/* {<div className='contentTabs'> */}
+                {/*     {media.length > 0 && <div onClick={(e) => this.selectTab('fashion', e)}>FASHION</div>} */}
+                {/*     {movies.length > 0 && <div onClick={(e) => this.selectTab('movies', e)}>MOVIES</div>} */}
+                    {/* {this.press().length >= 0 && <div onClick={(e) => this.selectTab('press', e)}>PRESS</div>} */}
+                  {/* </div>} */}
+                  {this.state.tab == 'fashion' &&
+                    <div className='contents'>
+                      {media.map((content, index) => (
+                        <MediaItem key={index} content={content} />
+                      ))}
+                    </div>}
+                    {/* {this.state.tab == 'movies' && */}
+                    {/*   <div className='contents'> */}
+                    {/*     {movies.map((movie, index) => ( */}
+                    {/*       <p> */}
+                    {/*         {movie.description} */}
+                    {/*       </p> */}
+                    {/*     ))} */}
+                    {/*   </div>} */}
+                      {/* {this.state.tab == 'press' && */}
+                      {/*   <div className='contents'> */}
+                      {/*   {press.map((press, index) => ( */}
+                      {/*     <a href={}> */}
+                      {/*       {press.description} */}
+                      {/*     </p> */}
+                      {/*   ))} */}
+                      {/* </div>} */}
+                    </div>
         }
       </div>
     );

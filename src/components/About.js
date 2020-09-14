6433788d0f25929cb7117ec3669dd92e6412eb57
Egor Chemokhonenko <egor.chemohonenko@ahrefs.com>
2020-09-14 12:15:32 +0300

    Update partners list

--- src/components/About.js
+++ src/components/About.js
@@ -30,12 +30,12 @@ export default class Contacts extends React.Component {
 
         <p>Clients:</p>
         <div className='partners'>
-          424, Adidas, Alchemist, Alyx, And Re Walker, Arctic Explorer, Balenciaga, Christian Dada, Comme des Garçons, Dirk Bikkembergs, DLT, Dumitrascu, Enfants Riches Déprimés, FCUK, Gosha Rubchinskiy, Gucci, Hed Mayner, Hood By Air, Jil Sander, Jourden, Junya Watanabe, KM20, Kenzo, Lanvin, Mackintosh, Marni, Nemen, Nike, OAMC, Off-White, Puma, Rick Owens, Sever, Situationist, SV Moscow, TSUM, The Sirius, Tigran Avetisyan, Tim Coppens, Umbro, Veronique Leroy, Vetements, Vien Atelier, Vlone, Walter Van Beirendonck, Ulyana Sergeenko, Yeezy, ZDDZ, Zara
+          424, Adidas, Alchemist, Alyx, And Re Walker, Arctic Explorer, Balenciaga, BMW, Christian Dada, Comme des Garçons, Dirk Bikkembergs, DLT, Dumitrascu, Enfants Riches Déprimés, FCUK, Gosha Rubchinskiy, Gucci, Hed Mayner, Hood By Air, Jil Sander, Jourden, Junya Watanabe, KM20, Kenzo, Lanvin, Mackintosh, Marni, Nemen, Nike, OAMC, Off-White, Puma, Rick Owens, Sever, Situationist, SV Moscow, TSUM, The Sirius, Tigran Avetisyan, Tim Coppens, Umbro, Veronique Leroy, Vetements, Vien Atelier, Vlone, Walter Van Beirendonck, Ulyana Sergeenko, Yeezy, ZDDZ, Zara
         </div>
 
         <p>Magazines:</p>
         <div className='partners'>
-          032с, Baron Magazine, Boycott Magazine, British Vogue, Buro 24/7, Dazed, Document Journal, Double Magazine, Dust Magazine, i-D Magazine, Intersection Magazine, Glamour Russia, Grind Magazine, GQ Russia, Harper’s Bazaar Russia, Elle Russia, Fucking Young!, Flacon Magazine, King Kong Magazine, L'Officiel Hommes Germany, Man About Town, Men's Uno, Modern Weekly China, Numéro Russia, Office Magazine, Purple Magazine, Re-Edition Mag, Sicky Magazine, Sleek Magazine, SNC Russia, The Blueprint, The Stylist France, The WILD Magazine, V Magazine, VMAN, Vogue Hommes, Vogue Italia, Vogue Russia
+          032с, Baron Magazine, Boycott Magazine, British Vogue, Buro 24/7, Dazed, Document Journal, Double Magazine, Dust Magazine, i-D Magazine, Intersection Magazine, Glamour Russia, Grind Magazine, GQ Russia, Harper’s Bazaar Russia, Elle Russia, Fucking Young!, Flacon Magazine, King Kong Magazine, L'Officiel Hommes Germany, Man About Town, Men's Uno, Modern Weekly China, Numéro Russia, ODDA Magazine, Office Magazine, Purple Magazine, Re-Edition Mag, Sicky Magazine, Sleek Magazine, SNC Russia, The Blueprint, The Stylist France, The WILD Magazine, V Magazine, VMAN, Vogue Hommes, Vogue Italia, Vogue Russia
         </div>
 
         <p className='footer'>Lumpen 2014-2020 ©</p>
